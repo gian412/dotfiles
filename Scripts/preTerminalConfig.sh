7059f0444bc241d5f933535e15f29e48423dce90
gian412 <gian.regis@gmail.com>
2021-01-13 00:03:19 +0100

    2021.01.13_00:03:19 from gianluca@garudadell

--- Scripts/preTerminalConfig.sh
+++ Scripts/preTerminalConfig.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# TODO: Check wich shell is in use.
 echo "\
 Hello $(whoami)!
 This script is the first of a serie of two. It will do the following for you:
@@ -30,25 +31,58 @@ Should the script start the installation [y/n]?"
 
 read isInstallationConfirmed
 
-if [ $isInstallationConfirmed = "n" ]
-then
+if [ $isInstallationConfirmed = "n" ]; then
     echo "Nothing won't be installed"
-elif [ $isInstallationConfirmed = "y" ]
-then
+elif [ $isInstallationConfirmed = "y" ]; then
     echo "Starting installation..."
 
-    # Install packages
-    sudo pacman -S zsh most flameshot chromium npm jre-openjdk jdk-openjdk ruby curl git
+    # Check for absent packages from Pacman
+    packages=("zsh" "most" "flameshot" "chromium" "npm" "ruby" "curl" "git")
+    packagesToInstall=""
 
-    # Install yay
-    cd /opt
-    sudo git clone https://aur.archlinux.org/yay-git.git
-    sudo chown -R $USER:$USER yay.git/
-    cd yay-git
-    makepkg -si
+    for pkg in ${packages[@]}; do
+        if ! command -v ${pkg} &> /dev/null; then
+            echo "${pkg} could not be found"
+            packagesToInstall="$packagesToInstall ${pkg}"
+        fi
+    done
 
-    # Install packages from AUR
-    yay -S undistract-me brave-bin
+    if ! command -v "javac" &> /dev/null; then
+        packagesToInstall="$packagesToInstall jdk-openjdk"
+    fi
+
+    # If there is any absent package, install it
+    if [ -n "$packagesToInstall" ]; then
+        sudo pacman -S $packagesToInstall
+    else
+        echo "Nothing to install from Pacman."
+    fi
+
+    # Check if yay is installed, otherwise install it
+    if ! command -v "yay" &> /dev/null; then
+        cd /opt
+        sudo git clone https://aur.archlinux.org/yay-git.git
+        sudo chown -R $USER:$USER yay.git/
+        cd yay-git
+        makepkg -si
+    fi
+
+    # Check for absent packages from AUR
+    if ! test -f "/usr/share/undistract-me/long-running.bash"; then
+        packagesToInstallAUR="$packagesToInstallAUR undistract-me"
+    fi
+
+
+    if ! command -v "brave" &> /dev/null; then
+        packagesToInstallAUR="$packagesToInstallAUR brave-bin"
+    fi
+
+    # If there is any absent package , install it
+    if [ -n "$packagesToInstallAUR" ]; then
+        yay -S $packagesToInstallAUR
+    else
+        echo "Nothing to install from AUR."
+    fi
 
     echo "Changing default shell to zsh..."
 
@@ -63,7 +97,12 @@ then
     git config --global pull.rebase false
 
     # Change default shell to zsh
-    chsh -s $(which zsh)
+    shell=`printenv | grep SHELL=`
+    if [ $shell = "SHELL=/usr/bin/fish" ]; then
+        chsh -s (which zsh)
+    else
+        chsh -s $(which zsh)
+    fi
 
 else
     echo "\
