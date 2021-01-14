f6ffc77bdbe0ba02a513fc262d20ce23767aa39f
gian412 <gian.regis@gmail.com>
2021-01-15 00:10:40 +0100

    2021.01.15_00:10:40 from gianluca@garudadell

--- Scripts/secondScript.sh
+++ Scripts/secondScript.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "\
-    Hello $(whoami)!
-    This script is the second of a serie of three.
-    Have you already run the first script? [y/n]"
+Hello $(whoami)!
+This script is the second of a serie of three.
+Have you already run the first script? [y/n]"
 
     read input
 
@@ -13,25 +13,34 @@ echo "\
     fi
 
     echo "\
-    This script will do the following for you:
-    
-    - Install Oh My Zsh, an open source, community-driven framework for managing zsh configuration. You can find more information at https://github.com/ohmyzsh/ohmyzsh
-    - Install Powerlevel10k, a theme for zsh. You can find more information at https://github.com/romkatv/powerlevel10k
-    
-    Should the script start the installation? [y/n]"
+This script will do the following for you:
 
-# Download and install Oh My Zsh
-echo ":: sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+- Install Oh My Zsh, an open source, community-driven framework for managing zsh configuration. You can find more information at https://github.com/ohmyzsh/ohmyzsh
+- Install Powerlevel10k, a theme for zsh. You can find more information at https://github.com/romkatv/powerlevel10k
 
-# Download and install Powerlevel10k
-echo ":: git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \${ZSH_CUSTOM:-\$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-echo ":: sed -i -e 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc"
-sed -i -e 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
+Should the script start the installation? [y/n]"
 
-echo"\
-Close your terminal, open it again and you can set your theme.
-Once your theme is setted, run the third script.
-See you later!"
-"
+read scriptChoice
+if [ $scriptChoice = "y" ]; then
+    # Download and install Oh My Zsh
+    echo ":: sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    # Download and install Powerlevel10k
+    echo ":: git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \${ZSH_CUSTOM:-\$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    echo ":: sed -i -e 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc"
+    sed -i -e 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
+
+    echo"\
+    Close your terminal, open it again and you can set your theme.
+    Once your theme is setted, run the third script.
+    See you later!"
+
+elif [ $scriptChoice = "n" ]; then
+    echo "Nothing won't be installed"
+else
+    echo"\
+Unrecognised user input: $scriptChoice.
+Script terminated."
+fi
