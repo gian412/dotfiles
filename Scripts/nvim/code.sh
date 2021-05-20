5637834954d4cd53b64dbc6e488f25e7360499bc
gian412 <r.gian@protonmail.com>
2021-05-20 08:52:10 +0200

    2021.05.20_08:52:10 from gianluca@garudaDell

--- Scripts/nvim/code.sh
+++ Scripts/nvim/code.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 # Remove actual configuration
-rm ~/.config/nvim/init.vim
-rm ~/.config/nvim/plugins.vim
-rm ~/.config/nvim/mappings.vim
+rm -f ~/.config/nvim/init.vim
+rm -f ~/.config/nvim/plugins.vim
+rm -f ~/.config/nvim/mappings.vim
 
+# if the folder does't exist, create it
+if [ ! -d ~/.config/nvim ] ; then
+  mkdir ~/.config/nvim
+fi
+
+# Copy configuration for coders
 cp ~/.nvimConfigs/code/cInit.vim ~/.config/nvim/init.vim
 cp ~/.nvimConfigs/code/cMappings.vim ~/.config/nvim/mappings.vim
 cp ~/.nvimConfigs/code/cPlugins.vim ~/.config/nvim/plugins.vim
--- Scripts/nvim/write.sh
+++ Scripts/nvim/write.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 # Remove actual configuration
-rm ~/.config/nvim/init.vim
-rm ~/.config/nvim/plugins.vim
-rm ~/.config/nvim/mappings.vim
+rm -f ~/.config/nvim/init.vim
+rm -f ~/.config/nvim/plugins.vim
+rm -f ~/.config/nvim/mappings.vim
 
+# if the folder does't exist, create it
+if [ ! -d ~/.config/nvim ] ; then
+  mkdir ~/.config/nvim
+fi
+
+# Copy configuration for writers
 cp ~/.nvimConfigs/write/wInit.vim ~/.config/nvim/init.vim
 cp ~/.nvimConfigs/write/wMappings.vim ~/.config/nvim/mappings.vim
 cp ~/.nvimConfigs/write/wPlugins.vim ~/.config/nvim/plugins.vim
