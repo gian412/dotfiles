a04c2dc003901b5ac479bda3a0eaec8daa218e9b
gian412 <r.gian@protonmail.com>
2022-02-20 04:15:17 +0100

    2022.02.20_04:15:17 from gianluca@garudaDell

--- Scripts/utils/xMonitorManagement.sh
+++ Scripts/utils/xMonitorManagement.sh
@@ -1,6 +1,6 @@
 if [[ "$(xrandr | grep '^DP1 connected')" != "" ]] && [[ "$(xrandr | grep '^HDMI-1-4 connected')" != "" ]]; then
     xrandr --output HDMI-1-4 --auto --primary
-    xrandr --output DP1 --auto --right-of DP1
+    xrandr --output DP1 --auto --left-of HDMI-1-4
     xrandr --output eDP1 --off
 fi
 
@@ -12,8 +12,8 @@ fi
 
 if [[ "$(xrandr | grep '^DP1 disconnected')" != "" ]] && [[ "$(xrandr | grep '^HDMI-1-4 connected')" != "" ]]; then
     xrandr --output DP1 --off
-    xrandr --output HDMI-1-4 --auto
-    xrandr --output eDP1 --auto --right-of HDMI-1-4
+    xrandr --output HDMI-1-4 --auto --primary
+    xrandr --output eDP1 --auto --left-of HDMI-1-4
 fi
 
 if [[ "$(xrandr | grep '^DP1 disconnected')" != "" ]] && [[ "$(xrandr | grep '^HDMI-1-4 disconnected')" != "" ]]; then
