1bc277384cc91626e810031fbbb36ee381f63df1
Gianluca <r.gian@protonmail.com>
2023-07-16 23:56:02 +0200

    fix(lsp): fix lsp mapping for errors

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -7,13 +7,20 @@ lsp.ensure_installed({
   'eslint',
 })
 
+lsp.set_sign_icons({
+  error = '✘',
+  warn = '▲',
+  hint = '⚑',
+  info = '»',
+})
+
 -- Remap for completion
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<CR>'] = cmp.mapping.confirm({ select = true }),
   ['<C-Space>'] = cmp.mapping.complete(),
 })
 
@@ -26,18 +33,22 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({buffer = bufnr})
   local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "gd", function () vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function () vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function () vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function () vim.lsp.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "gn", function () vim.lsp.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "gN", function () vim.lsp.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function () vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function () vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function () vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function () vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "gn", function () vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "gN", function () vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "gt", function () vim.lsp.buf.type_definition() end, opts)
+  vim.keymap.set("n", "<leader>ca", function () vim.lsp.buf.code_action() end, opts)
+  --vim.keymap.set("n", "K", function () vim.lsp.buf.hover() end, opts)
+  --vim.keymap.set("n", "gd", function () vim.lsp.buf.definition() end, opts)
+  --vim.keymap.set("n", "gD", function () vim.lsp.buf.declaration() end, opts)
+  --vim.keymap.set("n", "gi", function () vim.lsp.buf.implementation() end, opts)
+  --vim.keymap.set("n", "gr", function () vim.lsp.buf.references() end, opts)
+  --vim.keymap.set("n", "<F2>", function () vim.lsp.buf.rename() end, opts)
+  --vim.keymap.set("n", "<leader>vws", function () vim.lsp.buf.workspace_symbol() end, opts)
+  --vim.keymap.set("n", "<leader>vd", function () vim.diagnostic.open_float() end, opts)
+  --vim.keymap.set("i", "<C-h>", function () vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
