7c25604a8f5f3e5b6478ec37cf1e16df27cfba3b
Gianluca <r.gian@protonmail.com>
2023-07-25 01:50:30 +0200

    fix(monitors): fix monitor management

--- Scripts/utils/xMonitorManagement.sh
+++ Scripts/utils/xMonitorManagement.sh
@@ -1,24 +1,23 @@
-if [[ "$(xrandr | grep '^DP-1 connected')" != "" ]] && [[ "$(xrandr | grep '^HDMI-1-4 connected')" != "" ]]; then
-    xrandr --output HDMI-1-4 --auto --primary
-    xrandr --output DP-1 --auto --left-of HDMI-1-4
+if [[ "$(xrandr | grep '^DP-1 connected')" != "" ]] && [[ "$(xrandr | grep '^HDMI-1-0 connected')" != "" ]]; then
+    xrandr --output HDMI-1-0 --auto --primary
+    xrandr --output DP-1 --auto --left-of HDMI-1-0
     xrandr --output eDP-1 --off
 fi
 
-if [[ "$(xrandr | grep '^DP-1 connected')" != "" ]] && [[ "$(xrandr | grep '^HDMI-1-4 disconnected')" != "" ]]; then
-    xrandr --output HDMI-1-4 --off
+if [[ "$(xrandr | grep '^DP-1 connected')" != "" ]] && [[ "$(xrandr | grep '^HDMI-1-0 disconnected')" != "" ]]; then
+    xrandr --output HDMI-1-0 --off
     xrandr --output eDP-1 --auto --right-of DP1
     xrandr --output DP-1 --auto
 fi
 
-if [[ "$(xrandr | grep '^DP-1 disconnected')" != "" ]] && [[ "$(xrandr | grep '^HDMI-1-4 connected')" != "" ]]; then
+if [[ "$(xrandr | grep '^DP-1 disconnected')" != "" ]] && [[ "$(xrandr | grep '^HDMI-1-0 connected')" != "" ]]; then
     xrandr --output DP-1 --off
-    xrandr --output HDMI-1-4 --auto --primary
-    xrandr --output eDP-1 --auto --left-of HDMI-1-4
+    xrandr --output HDMI-1-0 --auto --primary
+    xrandr --output eDP-1 --auto --left-of HDMI-1-0
 fi
 
-if [[ "$(xrandr | grep '^DP-1 disconnected')" != "" ]] && [[ "$(xrandr | grep '^HDMI-1-4 disconnected')" != "" ]]; then
-    xrandr --output HDMI-1-4 --off
+if [[ "$(xrandr | grep '^DP-1 disconnected')" != "" ]] && [[ "$(xrandr | grep '^HDMI-1-0 disconnected')" != "" ]]; then
+    xrandr --output HDMI-1-0 --off
     xrandr --output DP-1 --off
     xrandr --output eDP-1 --auto
 fi
-
